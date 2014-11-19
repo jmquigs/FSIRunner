ff6e342c3df3acefe1bc210772140bb5da040310
John Quigley <jmquigs@gmail.com>
2014-11-18 21:01:59 -0500

    Tests: remove unused file

--- FSIRunner.Tests/TempTestDir/Runner.fsx
+++
@@ -1,8 +0,0 @@
-﻿#load "../../FSIRunner/Runner.fsx"
-
-let r = new FSIRunner.Runner()
-let pluginList = [ "Plugin1.fsx"; "Plugin2.fsx"] 
-let watchDirs = ["Dir1";"Dir2"]
-r.Watch pluginList watchDirs
-
-
